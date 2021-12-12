55cc07158e5247aa97b9f46a2f9b435094567838
Aurélien Rasse <aurelien.rasse28@gmail.com>
2021-12-12 16:06:28 +0100

    Deploying website

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { firebase } from "./firebase-config";
+
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyD5KB5NePsWDsUXMqIic1dldav1W3jblyw',
-    authDomain: 'joystuck.firebaseapp.com',
-    databaseURL: 'https://joystuck.firebaseio.com',
-    projectId: 'joystuck',
-    storageBucket: 'joystuck.appspot.com',
-    messagingSenderId: '612325519517',
-    appId: '1:612325519517:web:e0a724f26bad259b'
-  }
+  firebase: firebase
 };
 
 /*
---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebase = {
+    apiKey: 'AIzaSyD5KB5NePsWDsUXMqIic1dldav1W3jblyw',
+    authDomain: 'joystuck.firebaseapp.com',
+    databaseURL: 'https://joystuck.firebaseio.com',
+    projectId: 'joystuck',
+    storageBucket: 'joystuck.appspot.com',
+    messagingSenderId: '612325519517',
+    appId: '1:612325519517:web:e0a724f26bad259b'
+};
